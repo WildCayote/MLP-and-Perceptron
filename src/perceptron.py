382baf4ef49a9096149f7915ae5ba7fd56943e9e
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2025-01-07 18:28:14 +0300

    Feat: implemented initialize_weights, initialize_bias and __init__ methods

--- src/perceptron.py
+++ src/perceptron.py
@@ -1,12 +1,19 @@
-class Perceptron:
-    def __init__(self):
-        pass
-    
-    def initialize_weights(self):
-        pass
+import numpy as np
 
-    def initialize_biases(self):
-        pass
+class Perceptron:
+    def __init__(self, num_inputs:int, learning_rate:float = 0.1):
+        self.num_inputs = num_inputs
+        self.learning_rate = learning_rate
+
+        # initialize the weights
+        self.weights = self.initialize_weights()
+        self.bias = self.initialize_bias()
+
+    def initialize_weights(self):
+        return np.random.rand(self.num_inputs)
+
+    def initialize_bias(self):
+        return np.random.rand(1)
 
     def predict(self):
         pass
