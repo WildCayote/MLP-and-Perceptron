c8fc9547a88c05a9418377053d2c676363465c50
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2025-01-08 11:50:43 +0300

    Fix: enabled weight initialization and prediction that allows 0 and 1 hidden layer depth

--- src/mlp.py
+++ src/mlp.py
@@ -19,35 +19,53 @@ class MultiLayerPerceptron:
 
     def initialize_weights(self):
         # define the input weights
-        input_weights = np.random.rand(self.num_inputs, self.hidden_width)
-
+        if self.num_hidden > 0:
+            input_weights = np.random.rand(self.num_inputs, self.hidden_width)
+        else:
+            input_weights = np.random.rand(self.num_inputs, self.num_output)
+        
         # define the weights in the hidden layers
-        hidden_weights = []
-        for _ in range(self.num_hidden - 1):
-            hidden_weights.append(
-                np.random.rand(self.hidden_width, self.hidden_width)
-            )
-        hidden_weights = np.array(hidden_weights)
-
+        if self.num_hidden > 1:
+            hidden_weights = []
+            for _ in range(self.num_hidden - 1):
+                hidden_weights.append(
+                    np.random.rand(self.hidden_width, self.hidden_width)
+                )
+            hidden_weights = np.array(hidden_weights)
+        else:
+            hidden_weights = None
+        
         # define the weights between the last hidden layer and the output layer
-        output_weights = np.random.rand(self.hidden_width, self.num_output)
+        if self.num_hidden > 0:
+            output_weights = np.random.rand(self.hidden_width, self.num_output)
+        else:
+            output_weights = None
 
         return input_weights, hidden_weights, output_weights
 
     def initialize_biases(self):
         # define the biases in the input layer
-        input_biases = np.random.rand(self.hidden_width)
+        if self.num_hidden > 0:
+            input_biases = np.random.rand(self.hidden_width)
+        else: 
+            input_biases = np.random.rand(self.num_output)
 
         # defin the biases between the hidden layers
-        hidden_biases = []
-        for _ in range(self.num_hidden - 1):
-            hidden_biases.append(
-                np.random.rand((self.hidden_width))
-            )
-        hidden_biases = np.array(hidden_biases)
+        if self.num_hidden > 1:
+            hidden_biases = []
+            for _ in range(self.num_hidden - 1):
+                hidden_biases.append(
+                    np.random.rand((self.hidden_width))
+                )
+            hidden_biases = np.array(hidden_biases)
+        else:
+            hidden_biases = None
 
         # define the biases between the last hidden layer and the output layer
-        output_biases = np.random.rand(self.num_output)
+        if self.num_hidden > 0:
+            output_biases = np.random.rand(self.num_output)
+        else:
+            output_biases = None
 
         return input_biases, hidden_biases, output_biases
 
@@ -56,17 +74,23 @@ class MultiLayerPerceptron:
         z1 = input @ self.input_weights + self.input_biases
         z1 = self.activation_function(z1)
 
-        # hidden layers
-        z2 = None
-        for layer_idx in range(self.hidden_weights.shape[0]):
-            weights = self.hidden_weights[layer_idx]
-            biases = self.hidden_biases[layer_idx]
+        if self.num_hidden < 1:
+            return z1
 
-            if type(z2) != np.ndarray:
-                result = z1 @ weights + biases
-            else:
-                result = z2 @ weights + biases
-            z2 = self.activation_function(result)
+        # hidden layers
+        if self.num_hidden > 1:
+            z2 = None
+            for layer_idx in range(self.hidden_weights.shape[0]):
+                weights = self.hidden_weights[layer_idx]
+                biases = self.hidden_biases[layer_idx]
+
+                if type(z2) != np.ndarray:
+                    result = z1 @ weights + biases
+                else:
+                    result = z2 @ weights + biases
+                z2 = self.activation_function(result)
+        else:
+            z2 = z1
         
         # output layer
         z3 = z2 @ self.output_weights + self.output_biases
@@ -83,4 +107,4 @@ if __name__ == '__main__':
 
     test_x = np.array([[-1,2,3], [-1,-2,3], [-1,2,-3], [0,2,3]]) 
     test_y = np.array([1, 0, 0, 1])
-    mlp = MultiLayerPerceptron(num_inputs=3, num_hidden=2, num_output=1, hidden_width=4, activation_function=heaviside_step_func)
+    mlp = MultiLayerPerceptron(num_inputs=3, num_hidden=0, num_output=1, hidden_width=4, activation_function=heaviside_step_func)
